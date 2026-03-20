4a75cb89c9549a613dd5dac35cf3e76aab20182b
Vaduni Niranjan <vaduniniranjan@gmail.com>
2026-03-20 15:46:02 +0530

    Delete Java/bootcompany/src directory

--- Java/bootcompany/src/main/java/com/gal/dao/CabRepo.java
+++
@@ -1,7 +0,0 @@
-package com.gal.dao;
-
-import com.gal.model.Cab;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CabRepo extends JpaRepository<Cab, Integer> {
-}
--- Java/bootcompany/src/main/java/com/gal/service/AllocationService.java
+++
@@ -1,9 +0,0 @@
-package com.gal.service;
-
-import java.util.List;
-import com.gal.model.AllocationData;
-import com.gal.model.CabAllocation;
-
-public interface AllocationService {
-    List<CabAllocation> allocate(AllocationData allocData);
-}
